3bf736d9b032a50971471836eb0218b81278ba59
Haden <haden@hadenfletcher.com>
2019-09-01 03:32:54 -0500

    add home redirect

--- src/router.js
+++ src/router.js
@@ -9,6 +9,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: () => import('./views/Home.vue')
+        },
+        {
+            path: '*',
+            name: 'home-redirect',
+            redirect: '/'
         }
     ]
 })
